0c951d927c5f46b29f91c2fc13b18a9c7f8916d4
HlibChan <chansky.gleb@gmail.com>
2023-02-11 21:19:57 +0200

    lesson2

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -6,37 +6,81 @@ import {
   HttpCode,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { News, NewsService } from './news.sevice';
+import { CommentsService } from '../comments/comments.service';
+import { renderNewsAll } from '../views/news/allNews';
+import { renderTemplate } from '../views/template';
+import { NewsPage } from '../views/news/news';
+import { CommentListView } from '../views/news/comments/comments';
 
 @Controller('news')
 export class NewsController {
-  constructor(private readonly newsService: NewsService) {}
+  constructor(
+    private readonly newsService: NewsService,
+    private readonly commentService: CommentsService,
+  ) {}
 
-  @Get('/all')
+  @Get('/')
+  @HttpCode(200)
+  allNewsView() {
+    const news = this.newsService.allNews();
+    const content = renderNewsAll(news);
+    return renderTemplate(content, {
+      title: 'News list',
+      description: 'news',
+    });
+  }
+
+  @Get('/:id')
+  @HttpCode(200)
+  getNewsView(@Param('id') idOrig: string): string {
+    const id = parseInt(idOrig);
+    const news = this.newsService.find(id);
+    const comments = this.commentService.find(id) || [];
+    const readyComments = CommentListView(comments);
+    const readyNews = NewsPage(news, readyComments);
+    return renderTemplate(readyNews, {
+      title: `News #${id}`,
+      description: 'news',
+    });
+  }
+
+  @Get('/api/all')
   @HttpCode(200)
   allNews(): News[] {
     return this.newsService.allNews();
   }
 
-  @Get('/:id')
+  @Get('/api/:id')
   @HttpCode(200)
   get(@Param('id') idOrig: string): News {
     const id = parseInt(idOrig);
-    return this.newsService.find(id);
+    const news = this.newsService.find(id);
+    const comments = this.commentService.find(id);
+    return {
+      ...news,
+      comments,
+    };
   }
 
-  @Post()
+  @Put('/api')
+  edit(@Body() news: News): News[] {
+    return this.newsService.edit(news);
+  }
+
+  @Post('/api')
   @HttpCode(200)
   create(@Body() news: News): News[] {
     return this.newsService.create(news);
   }
 
-  @Delete('/:id')
+  @Delete('/api/:id')
   @HttpCode(200)
   delete(@Param('id') idOrig: string): string {
     const id = parseInt(idOrig);
     const isRemoved = this.newsService.remove(id);
-    return isRemoved ? 'Новость удалена' : 'Передан неверный id';
+    return isRemoved ? 'News have been removed' : 'Incorrect if';
   }
 }
